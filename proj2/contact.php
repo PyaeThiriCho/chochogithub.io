1f1df5bcf35d067c470ff9373b2826b8d253ba1e
PyaeThiriCho <pyaethiricho4@gmail.com>
2025-05-19 06:35:07 -0700

    project3

---
+++ proj2/contact.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</tite>
+</head>
+<body>
+    <!--Navbar-->
+    <?php include 'navbar.php'; ?>
+
+    <!--Main Contant-->
+    <h1>About page</h1>
+    <h1>Welcome to our website</h1>
+    <p>This is the About page of our contact</p>
+
+    <!--Footer-->
+    <?php include 'footer.php'; ?>
+</body>
+</html>
